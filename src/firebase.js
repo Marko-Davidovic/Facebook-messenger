eb9908fe88b9d7573b08d275b040c68a40b3d680
Marko Davidovic <davidovic-m@hotmail.com>
2021-05-10 12:53:57 -0400

    firestore update

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,12 @@ import firebase from "firebase"
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyAlBH9zOcnLEqibvfVCJ_fId2ShkCUBtCI",
-  authDomain: "facebook-messenger-cea89.firebaseapp.com",
-  databaseURL: "https://facebook-messenger-cea89.firebaseio.com",
-  projectId: "facebook-messenger-cea89",
-  storageBucket: "facebook-messenger-cea89.appspot.com",
-  messagingSenderId: "99544821500",
-  appId: "1:99544821500:web:142944bb576bb2819c7179",
-  measurementId: "G-RVJY140E6Q"
+  apiKey: "AIzaSyBMJYDOFzWHMBEl3Q7u251VWxz4HSuWN6s",
+    authDomain: "facebook-messanger-204d9.firebaseapp.com",
+    projectId: "facebook-messanger-204d9",
+    storageBucket: "facebook-messanger-204d9.appspot.com",
+    messagingSenderId: "262391985204",
+    appId: "1:262391985204:web:d53eb67c2a38c92a380661"
 })
 
   const db = firebaseApp.firestore();
